4c633f106adbc8443bc3f467ef739986b11835c7
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-12-15 21:48:02 +0530

    CHG | USer Functions

--- src/db/users.ts
+++ src/db/users.ts
@@ -25,9 +25,7 @@ export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne
     'authentication.sessionToken' : sessionToken,
 });
 export const getUserById = (id: string) => UserModel.findById(id);
-
 export const createUser = (values: Record<string, any>) => new UserModel(values)
     .save().then((user: any) => user.toObject());
-
 export const deleteUserById = (id: string) => UserModel.findOneAndDelete({_id: id});
 export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
